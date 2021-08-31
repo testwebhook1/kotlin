5649a3fabeff7677854fbf43a9a066cfc4f9f6c2
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2021-08-31 17:04:31 +0300

    ++ repro success (???)

--- compiler/fir/analysis-tests/testData/resolveWithStdlib/intellij/accessInSetter.kt
+++ compiler/fir/analysis-tests/testData/resolveWithStdlib/intellij/accessInSetter.kt
@@ -1,23 +1,34 @@
 // FULL_JDK
+// MODULE: m1
+// FILE: m1.kt
 
 import javax.swing.JList
 
-class DrawableGrid : JList<String>()
+class DrawableGrid : JList<String>() {
+    override fun setSelectionInterval(anchor: Int, lead: Int) {
+        super.setSelectionInterval(anchor, lead)
+    }
+}
 
-class My {
+// MODULE: m2(m1)
+// FILE: m2.kt
+
+class My(private val originalValue: String?) {
     private val drawableGrid = createDrawableGrid()
 
-    private var useAll = false
+    private var useAll = originalValue == null || isSampleValueAll(originalValue)
         set(value) {
             field = value
-            drawableGrid.isEnabled = !value
-            if (value) drawableGrid.clearSelection() else drawableGrid.selectedIndex = 0
+            drawableGrid.<!UNRESOLVED_REFERENCE!>isEnabled<!> = !value
+            if (value) drawableGrid.<!UNRESOLVED_REFERENCE!>clearSelection<!>() else drawableGrid.<!UNRESOLVED_REFERENCE!>selectedIndex<!> = 0
         }
 
-    private fun createDrawableGrid() = DrawableGrid().apply {
-        isOpaque = false
-        visibleRowCount = 3
-        addListSelectionListener { _ ->
+    private fun createDrawableGrid() = DrawableGrid().<!OVERLOAD_RESOLUTION_AMBIGUITY!>apply<!> {
+        <!UNRESOLVED_REFERENCE!>isOpaque<!> = false
+        <!UNRESOLVED_REFERENCE!>visibleRowCount<!> = 3
+        <!UNRESOLVED_REFERENCE!>addListSelectionListener<!> { _ ->
         }
     }
+
+    private fun isSampleValueAll(value: String?) = value?.endsWith(']')?.not() ?: false
 }
