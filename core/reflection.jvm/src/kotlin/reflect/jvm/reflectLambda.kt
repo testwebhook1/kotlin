3e9ce3fc8cbaede8a1e60c931f72e07705782750
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2021-09-03 08:24:49 +0300

    ++ Drop incorrect OptIn marker targets from stdlib

--- core/reflection.jvm/src/kotlin/reflect/jvm/reflectLambda.kt
+++ core/reflection.jvm/src/kotlin/reflect/jvm/reflectLambda.kt
@@ -64,12 +64,8 @@ fun <R> Function<R>.reflect(): KFunction<R>? {
     CLASS,
     ANNOTATION_CLASS,
     PROPERTY,
-    FIELD,
-    LOCAL_VARIABLE,
-    VALUE_PARAMETER,
     CONSTRUCTOR,
     FUNCTION,
-    PROPERTY_GETTER,
     PROPERTY_SETTER,
     TYPEALIAS
 )
