caae3cdef4693c184741dafceee16d55bd9c0cc7
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2021-09-03 07:26:56 +0300

    Drop incorrect OptIn marker targets from stdlib

--- libraries/stdlib/unsigned/src/kotlin/annotations/Unsigned.kt
+++ libraries/stdlib/unsigned/src/kotlin/annotations/Unsigned.kt
@@ -23,7 +23,7 @@ import kotlin.internal.RequireKotlinVersionKind
 @Experimental(level = Experimental.Level.WARNING)
 @RequiresOptIn(level = RequiresOptIn.Level.WARNING)
 @MustBeDocumented
-@Target(CLASS, ANNOTATION_CLASS, PROPERTY, FIELD, LOCAL_VARIABLE, VALUE_PARAMETER, CONSTRUCTOR, FUNCTION, PROPERTY_GETTER, PROPERTY_SETTER, TYPEALIAS)
+@Target(CLASS, ANNOTATION_CLASS, PROPERTY, CONSTRUCTOR, FUNCTION, PROPERTY_SETTER, TYPEALIAS)
 @Retention(AnnotationRetention.BINARY)
 @RequireKotlin("1.2.50", versionKind = RequireKotlinVersionKind.COMPILER_VERSION)
 public annotation class ExperimentalUnsignedTypes
