caf365b3e3358ca378d9cae191eae9fcb2e99798
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2021-09-13 14:07:35 +0300

    ~ Better test

--- compiler/testData/codegen/box/jvmField/fromDynamicPlugins.kt
+++ compiler/testData/codegen/box/jvmField/fromDynamicPlugins.kt
@@ -2,22 +2,33 @@
 // WITH_RUNTIME
 
 // MODULE: m1
-// FILE: m1.kt
+// FILE: ContainerDescriptor.java
 
-class SomeClass {
-    @JvmField var foo: MutableList<Name>? = null
+package same;
+
+import org.jetbrains.annotations.Nullable;
+
+public final class ContainerDescriptor {
+    @Nullable String extensionPoint = "OK";
 }
 
-class Name(@JvmField val name: String)
-
 // MODULE: m2(m1)
-// FILE: m2.kt
+// FILE: DynamicPlugins.kt
 
-fun box(): String {
-    val sc = SomeClass()
-    sc.foo = mutableListOf(Name("OK"))
-    sc.foo?.let {
-        return@box it[0].name
+package same
+
+object DynamicPlugins {
+    fun box(): String {
+        return doIt()
+    }
+}
+
+private fun doIt(): String {
+    val cd = ContainerDescriptor()
+    cd.extensionPoint?.let {
+        return@doIt it
     }
     return "FAIL"
 }
+
+fun box() = DynamicPlugins.box()
