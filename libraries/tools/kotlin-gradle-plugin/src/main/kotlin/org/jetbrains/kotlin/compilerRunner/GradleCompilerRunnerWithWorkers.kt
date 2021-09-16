21bc405674e71357f627a5944d8ec2b2436f9c70
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2021-09-16 10:52:15 +0200

    Restore tasks outputs to pre-execution state in worker.
    Workers usually execute later than task action and does not raise
    exception to the task. Such behaviour skips task outputs restoration to
    the pre-execution state.
    
    Kotlin compiler on incremental compilation error leaves 'dirty*' files
    in the build output, forcing Gradle to run non-incrementally on the
    next build. Fixed it by moving task outputs reset logic into worker as
    well.
    
    ^KT-46406 In Progress

--- libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/compilerRunner/GradleCompilerRunnerWithWorkers.kt
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/compilerRunner/GradleCompilerRunnerWithWorkers.kt
@@ -5,13 +5,17 @@
 
 package org.jetbrains.kotlin.compilerRunner
 
+import org.gradle.api.GradleException
+import org.gradle.api.file.ConfigurableFileCollection
+import org.gradle.api.provider.MapProperty
 import org.gradle.api.provider.Property
 import org.gradle.workers.WorkAction
 import org.gradle.workers.WorkParameters
 import org.gradle.workers.WorkQueue
 import org.gradle.workers.WorkerExecutor
-import org.jetbrains.kotlin.gradle.logging.kotlinDebug
+import org.jetbrains.kotlin.build.report.metrics.BuildMetricsReporter
 import org.jetbrains.kotlin.gradle.tasks.GradleCompileTaskProvider
+import org.jetbrains.kotlin.gradle.tasks.TaskOutputsBackup
 import java.io.File
 
 /**
@@ -21,28 +25,53 @@ internal class GradleCompilerRunnerWithWorkers(
     taskProvider: GradleCompileTaskProvider,
     jdkToolsJar: File?,
     kotlinDaemonJvmArgs: List<String>?,
+    buildMetrics: BuildMetricsReporter,
     private val workerExecutor: WorkerExecutor
-) : GradleCompilerRunner(taskProvider, jdkToolsJar, kotlinDaemonJvmArgs) {
-    override fun runCompilerAsync(workArgs: GradleKotlinCompilerWorkArguments): WorkQueue {
-        loggerProvider.kotlinDebug { "Starting Kotlin compiler work from task '${pathProvider}'" }
+) : GradleCompilerRunner(taskProvider, jdkToolsJar, kotlinDaemonJvmArgs, buildMetrics) {
+    override fun runCompilerAsync(
+        workArgs: GradleKotlinCompilerWorkArguments,
+        taskOutputsBackup: TaskOutputsBackup?
+    ): WorkQueue {
 
         val workQueue = workerExecutor.noIsolation()
         workQueue.submit(GradleKotlinCompilerWorkAction::class.java) {
             it.compilerWorkArguments.set(workArgs)
+            if (taskOutputsBackup != null) {
+                it.taskOutputs.from(taskOutputsBackup.outputs)
+                it.taskOutputsSnapshot.set(taskOutputsBackup.previousOutputs)
+            }
         }
         return workQueue
     }
 
     internal abstract class GradleKotlinCompilerWorkAction
         : WorkAction<GradleKotlinCompilerWorkParameters> {
+
         override fun execute() {
-            GradleKotlinCompilerWork(
-                parameters.compilerWorkArguments.get()
-            ).run()
+            try {
+                GradleKotlinCompilerWork(
+                    parameters.compilerWorkArguments.get()
+                ).run()
+            } catch (e: GradleException) {
+                if (parameters.taskOutputsSnapshot.isPresent) {
+                    val taskOutputsBackup = TaskOutputsBackup(
+                        parameters.taskOutputs,
+                        parameters.taskOutputsSnapshot.get()
+                    )
+                    // Measuring in worker task outputs restore time will not work
+                    // Currently build metrics are collected and persisted right after task action execution
+                    // while worker could be executed at a later time
+                    taskOutputsBackup.restoreOutputs()
+                }
+
+                throw e
+            }
         }
     }
 
     internal interface GradleKotlinCompilerWorkParameters : WorkParameters {
         val compilerWorkArguments: Property<GradleKotlinCompilerWorkArguments>
+        val taskOutputs: ConfigurableFileCollection
+        val taskOutputsSnapshot: MapProperty<File, Array<Byte>>
     }
 }
--- libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/tasks/Tasks.kt
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/tasks/Tasks.kt
@@ -92,8 +92,8 @@ abstract class AbstractKotlinCompileTool<T : CommonToolArguments>
     }
 
     @get:Internal
-    override val metrics: BuildMetricsReporter =
-        BuildMetricsReporterImpl()
+    override val metrics: Property<BuildMetricsReporter> = project.objects
+        .property(BuildMetricsReporterImpl())
 
     /**
      * By default, should be set by plugin from [COMPILER_CLASSPATH_CONFIGURATION_NAME] configuration.
@@ -315,7 +315,12 @@ abstract class AbstractKotlinCompile<T : CommonCompilerArguments> : AbstractKotl
     internal open val compilerRunner: Provider<GradleCompilerRunner> =
         objects.propertyWithConvention(
             gradleCompileTaskProvider.map {
-                GradleCompilerRunner(it, null, normalizedKotlinDaemonJvmArguments.orNull)
+                GradleCompilerRunner(
+                    it,
+                    null,
+                    normalizedKotlinDaemonJvmArguments.orNull,
+                    metrics.get()
+                )
             }
         )
 
@@ -323,7 +328,8 @@ abstract class AbstractKotlinCompile<T : CommonCompilerArguments> : AbstractKotl
 
     @TaskAction
     fun execute(inputChanges: InputChanges) {
-        metrics.measure(BuildTime.GRADLE_TASK_ACTION) {
+        val buildMetrics = metrics.get()
+        buildMetrics.measure(BuildTime.GRADLE_TASK_ACTION) {
             systemPropertiesService.get().startIntercept()
             CompilerSystemProperties.KOTLIN_COMPILER_ENVIRONMENT_KEEPALIVE_PROPERTY.value = "true"
 
@@ -332,7 +338,7 @@ abstract class AbstractKotlinCompile<T : CommonCompilerArguments> : AbstractKotl
             // To prevent this, we backup outputs before incremental build and restore when exception is thrown
             val outputsBackup: TaskOutputsBackup? =
                 if (isIncrementalCompilationEnabled() && inputChanges.isIncremental)
-                    metrics.measure(BuildTime.BACKUP_OUTPUT) {
+                    buildMetrics.measure(BuildTime.BACKUP_OUTPUT) {
                         TaskOutputsBackup(allOutputFiles())
                     }
                 else null
@@ -343,15 +349,18 @@ abstract class AbstractKotlinCompile<T : CommonCompilerArguments> : AbstractKotl
                 clearLocalState("Task cannot run incrementally")
             }
 
-            try {
-                executeImpl(inputChanges)
-            } catch (t: Throwable) {
-                if (outputsBackup != null) {
-                    metrics.measure(BuildTime.RESTORE_OUTPUT_FROM_BACKUP) {
-                        outputsBackup.restoreOutputs()
-                    }
-                }
-                throw t
+            executeImpl(inputChanges, outputsBackup)
+            buildMetrics.measure(BuildTime.CALCULATE_OUTPUT_SIZE) {
+                buildMetrics.addMetric(
+                    BuildPerformanceMetric.SNAPSHOT_SIZE,
+                    taskBuildDirectory.file("build-history.bin").get().asFile.length() +
+                            taskBuildDirectory.file("last-build.bin").get().asFile.length() +
+                            taskBuildDirectory.file("abi-snapshot.bin").get().asFile.length()
+                )
+                buildMetrics.addMetric(
+                    BuildPerformanceMetric.OUTPUT_SIZE,
+                    taskBuildDirectory.dir("caches-jvm").get().asFileTree.files.filter { it.isFile }.map { it.length() }.sum()
+                )
             }
         }
     }
@@ -369,7 +378,10 @@ abstract class AbstractKotlinCompile<T : CommonCompilerArguments> : AbstractKotl
             commonSourceSet
         )
 
-    private fun executeImpl(inputChanges: InputChanges) {
+    private fun executeImpl(
+        inputChanges: InputChanges,
+        taskOutputsBackup: TaskOutputsBackup?
+    ) {
         val sourceRoots = getSourceRoots()
         val allKotlinSources = sourceRoots.kotlinSourceFiles
 
@@ -386,7 +398,12 @@ abstract class AbstractKotlinCompile<T : CommonCompilerArguments> : AbstractKotl
 
         sourceRoots.log(this.name, logger)
         taskBuildDirectory.get().asFile.mkdirs()
-        callCompilerAsync(args, sourceRoots, inputChanges)
+        callCompilerAsync(
+            args,
+            sourceRoots,
+            inputChanges,
+            taskOutputsBackup
+        )
     }
 
     protected fun getChangedFiles(
@@ -420,7 +437,12 @@ abstract class AbstractKotlinCompile<T : CommonCompilerArguments> : AbstractKotl
      * Compiler might be executed asynchronously. Do not do anything requiring end of compilation after this function is called.
      * @see [GradleKotlinCompilerWork]
      */
-    internal abstract fun callCompilerAsync(args: T, sourceRoots: SourceRoots, inputChanges: InputChanges)
+    internal abstract fun callCompilerAsync(
+        args: T,
+        sourceRoots: SourceRoots,
+        inputChanges: InputChanges,
+        taskOutputsBackup: TaskOutputsBackup?
+    )
 
     @get:Input
     internal val multiPlatformEnabled: Property<Boolean> = objects.property(Boolean::class.java)
@@ -623,6 +645,7 @@ abstract class KotlinCompile @Inject constructor(
                     it,
                     toolchain.currentJvmJdkToolsJar.orNull,
                     normalizedKotlinDaemonJvmArguments.orNull,
+                    metrics.get(),
                     workerExecutor
                 )
             })
@@ -673,7 +696,12 @@ abstract class KotlinCompile @Inject constructor(
         validateKotlinAndJavaHasSameTargetCompatibility(args)
     }
 
-    override fun callCompilerAsync(args: K2JVMCompilerArguments, sourceRoots: SourceRoots, inputChanges: InputChanges) {
+    override fun callCompilerAsync(
+        args: K2JVMCompilerArguments,
+        sourceRoots: SourceRoots,
+        inputChanges: InputChanges,
+        taskOutputsBackup: TaskOutputsBackup?
+    ) {
         sourceRoots as SourceRoots.ForJvm
 
         val messageCollector = GradlePrintingMessageCollector(logger, args.allWarningsAsErrors)
@@ -718,7 +746,8 @@ abstract class KotlinCompile @Inject constructor(
             javaPackagePrefix,
             args,
             environment,
-            defaultKotlinJavaToolchain.get().providedJvm.get().javaHome
+            defaultKotlinJavaToolchain.get().providedJvm.get().javaHome,
+            taskOutputsBackup
         )
     }
 
@@ -922,6 +951,7 @@ abstract class Kotlin2JsCompile @Inject constructor(
                     it,
                     null,
                     normalizedKotlinDaemonJvmArguments.orNull,
+                    metrics.get(),
                     workerExecutor
                 )
             }
@@ -1030,7 +1060,12 @@ abstract class Kotlin2JsCompile @Inject constructor(
     override val incrementalProps: List<FileCollection>
         get() = super.incrementalProps + listOf(friendDependencies)
 
-    override fun callCompilerAsync(args: K2JSCompilerArguments, sourceRoots: SourceRoots, inputChanges: InputChanges) {
+    override fun callCompilerAsync(
+        args: K2JSCompilerArguments,
+        sourceRoots: SourceRoots,
+        inputChanges: InputChanges,
+        taskOutputsBackup: TaskOutputsBackup?
+    ) {
         sourceRoots as SourceRoots.KotlinOnly
 
         logger.debug("Calling compiler")
@@ -1082,7 +1117,8 @@ abstract class Kotlin2JsCompile @Inject constructor(
             sourceRoots.kotlinSourceFiles.files.toList(),
             commonSourceSet.toList(),
             args,
-            environment
+            environment,
+            taskOutputsBackup
         )
     }
 }
